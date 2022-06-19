110144d51a89bfac2bd16952fb6b0a776f75fbe2
M.J <janelidze.marita@kiu.edu.ge>
2022-06-19 20:24:25 +0400

    Initial Commit

--- Registration/Register.js
+++ Registration/Register.js
@@ -1,15 +1,15 @@
 function checkPassword(form) {
     let password1 = form.psw.value;
-   let  password2 = form.psw-repeat.value;
+    let password2 = form.psw - repeat.value;
 
     if (password1 == '')
-        alert ("Please enter Password");
+        alert("Please enter Password");
 
     else if (password2 == '')
-        alert ("Please repeat password");
+        alert("Please repeat password");
 
     else if (password1 != password2) {
-        alert ("\nPassword did not match: Please try again...")
+        alert("\nPassword did not match: Please try again...")
         return false;
     }
 }
---
+++ UserPage/UserScript.js
@@ -0,0 +1,3 @@
+const deteleItem = function (id){
+
+}
