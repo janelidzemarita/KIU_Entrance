38fc0b48d3c99f4878c5c8ede28ed1de36042373
gogita20021 <82566373+gogita20021@users.noreply.github.com>
2022-06-19 20:29:11 +0400

    Update Register.js

--- Registration/Register.js
+++ Registration/Register.js
@@ -1,15 +1,27 @@
-function checkPassword(form) {
-    let password1 = form.psw.value;
-    let password2 = form.psw - repeat.value;
+function check() {
+    let password1 = document.getElementById("psw").value;
+   let password2 = document.getElementById("psw_repeat.value").value;
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let ID = document.getElementById("ID_number").value;
+    let number = document.getElementById("phone_number").value;
 
-    if (password1 == '')
-        alert("Please enter Password");
-
-    else if (password2 == '')
-        alert("Please repeat password");
-
-    else if (password1 != password2) {
-        alert("\nPassword did not match: Please try again...")
+    if (password1 === '' || password2 === '' ||!(/^[A-z ]+$/.test(name)) ||
+    !(/^[A-z ]+$/.test(surname)) || ID.length !== 11 || number.length !==9
+    || name==='' || surname==='' || ID===''|| number===''){
+        alert ("Please enter correct information");
         return false;
+    }
+    else if (password1 !== password2) {
+        alert ("\nPassword did not match: Please try again...")
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function create(state){
+    if (state) {
+        //create user
     }
 }
